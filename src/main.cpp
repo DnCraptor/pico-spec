9a22f2ffd16de3321144d66612c8367eedf4240c
drew.po28@gmail.com <drew.po28@gmail.com>
2026-04-13 17:49:08 +0300

    Refactor WASD key handling to improve readability and maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -654,23 +654,31 @@ void kbdExtraMapping(fabgl::VirtualKey virtualKey, bool pressed) {
             break;
         }
         case fabgl::VirtualKey::VK_D: {
-            if (Config::wasd) joyPushData(fabgl::VirtualKey::VK_DPAD_RIGHT, pressed);
-            joyPushData(fabgl::VirtualKey::VK_MENU_RIGHT, pressed);
+            if (Config::wasd) {
+                joyPushData(fabgl::VirtualKey::VK_DPAD_RIGHT, pressed);
+                joyPushData(fabgl::VirtualKey::VK_MENU_RIGHT, pressed);
+            }
             break;
         }
         case fabgl::VirtualKey::VK_W: {
-            if (Config::wasd) joyPushData(fabgl::VirtualKey::VK_DPAD_UP, pressed);
-            joyPushData(fabgl::VirtualKey::VK_MENU_UP, pressed);
+            if (Config::wasd) {
+                joyPushData(fabgl::VirtualKey::VK_DPAD_UP, pressed);
+                joyPushData(fabgl::VirtualKey::VK_MENU_UP, pressed);
+            }
             break;
         }
         case fabgl::VirtualKey::VK_A: {
-            if (Config::wasd) joyPushData(fabgl::VirtualKey::VK_DPAD_LEFT, pressed);
-            joyPushData(fabgl::VirtualKey::VK_MENU_LEFT, pressed);
+            if (Config::wasd) {
+                joyPushData(fabgl::VirtualKey::VK_DPAD_LEFT, pressed);
+                joyPushData(fabgl::VirtualKey::VK_MENU_LEFT, pressed);
+            }
             break;
         }
         case fabgl::VirtualKey::VK_S: {
-            if (Config::wasd) joyPushData(fabgl::VirtualKey::VK_DPAD_DOWN, pressed);
-            joyPushData(fabgl::VirtualKey::VK_MENU_DOWN, pressed);
+            if (Config::wasd) {
+                joyPushData(fabgl::VirtualKey::VK_DPAD_DOWN, pressed);
+                joyPushData(fabgl::VirtualKey::VK_MENU_DOWN, pressed);
+            }
             break;
         }
         case fabgl::VirtualKey::VK_KP_ENTER: { // VK_KP_ENTER
