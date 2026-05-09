c96e9595b4c573e7c74d1761b16153f52d5b04aa
drew.po28@gmail.com <drew.po28@gmail.com>
2026-05-09 17:05:04 +0300

    Refactor gamepad input handling in tuh_xinput_report_received_cb for improved clarity and efficiency

--- src/joy_hid.cpp
+++ src/joy_hid.cpp
@@ -24,141 +24,89 @@ usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count) {
 #include "ESPectrum.h"
 #include "Video.h"
 
+// Mapping mirrors hid_app.cpp process_ds4_gamepad/process_hid_gamepad:
+// D-pad+stick → Kempston (VK_DPAD_*) + OSD nav (VK_MENU_*), no cursor
+// overlay. A=Fire, B=AltFire, Start=F1/Start, Back=Select, X/Y/LB/RB →
+// VK_JOY_X/Y/Z/C for joydef rebinding. Left analog stick falls back to
+// D-pad direction so games requiring Kempston work without the hat.
 void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
     auto xid_itf = (xinputh_interface_t *)report;
     const xinput_gamepad_t* p = &xid_itf->pad;
 
-        if ((p->wButtons & XINPUT_GAMEPAD_A) && !gamepad1_bits.a) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_ENTER, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_FIRE, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_0 : fabgl::VirtualKey::VK_RETURN, true);
-        }
-        else if (!(p->wButtons & XINPUT_GAMEPAD_A) && gamepad1_bits.a) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_ENTER, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_FIRE, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_0 : fabgl::VirtualKey::VK_RETURN, false);
-        }
+    const uint16_t btns = p->wButtons;
 
-        if ((p->wButtons & XINPUT_GAMEPAD_B) && !gamepad1_bits.b) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_ENTER, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_ALTFIRE, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_1 : fabgl::VirtualKey::VK_SPACE, true);
-        }
-        else if (!(p->wButtons & XINPUT_GAMEPAD_B) && gamepad1_bits.b) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_ENTER, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_ALTFIRE, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_1 : fabgl::VirtualKey::VK_SPACE, false);
-        }
+    bool dpad_up    = (btns & XINPUT_GAMEPAD_DPAD_UP)    != 0;
+    bool dpad_down  = (btns & XINPUT_GAMEPAD_DPAD_DOWN)  != 0;
+    bool dpad_left  = (btns & XINPUT_GAMEPAD_DPAD_LEFT)  != 0;
+    bool dpad_right = (btns & XINPUT_GAMEPAD_DPAD_RIGHT) != 0;
 
+    // Stick fallback: ~25% deflection threshold (sThumb is int16, ±32767).
+    bool stick_up    = p->sThumbLY >  8000;
+    bool stick_down  = p->sThumbLY < -8000;
+    bool stick_left  = p->sThumbLX < -8000;
+    bool stick_right = p->sThumbLX >  8000;
 
-    gamepad1_bits.a = p->wButtons & XINPUT_GAMEPAD_A;
-    gamepad1_bits.b = p->wButtons & XINPUT_GAMEPAD_B;
+    bool up    = dpad_up    || stick_up;
+    bool down  = dpad_down  || stick_down;
+    bool left  = dpad_left  || stick_left;
+    bool right = dpad_right || stick_right;
 
+    bool btn_a     = (btns & XINPUT_GAMEPAD_A) != 0;
+    bool btn_b     = (btns & XINPUT_GAMEPAD_B) != 0;
+    bool btn_x     = (btns & XINPUT_GAMEPAD_X) != 0;
+    bool btn_y     = (btns & XINPUT_GAMEPAD_Y) != 0;
+    bool btn_lb    = (btns & XINPUT_GAMEPAD_LEFT_SHOULDER)  != 0;
+    bool btn_rb    = (btns & XINPUT_GAMEPAD_RIGHT_SHOULDER) != 0;
+    bool btn_start = (btns & XINPUT_GAMEPAD_START) != 0;
+    bool btn_back  = (btns & XINPUT_GAMEPAD_BACK)  != 0;
 
-        if ((p->wButtons & XINPUT_GAMEPAD_GUIDE) && !gamepad1_bits.start && !gamepad1_bits.select) {
-            gamepad1_bits.start = true;
-            gamepad1_bits.select = true;
-            joyPushData(fabgl::VirtualKey::VK_F1, true);
-        }
-        else if (!(p->wButtons & XINPUT_GAMEPAD_GUIDE) && gamepad1_bits.start && gamepad1_bits.select) {
-            gamepad1_bits.start = false;
-            gamepad1_bits.select = false;
-            joyPushData(fabgl::VirtualKey::VK_F1, false);
-        }
-
-
-        if ((p->wButtons & XINPUT_GAMEPAD_BACK) && !gamepad1_bits.select) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_BS, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_SELECT, true);
-        }
-        else if (!(p->wButtons & XINPUT_GAMEPAD_BACK) && gamepad1_bits.select) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_BS, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_SELECT, false);
-        }
-    gamepad1_bits.select = p->wButtons & XINPUT_GAMEPAD_BACK;
-        if ((p->wButtons & XINPUT_GAMEPAD_START) && !gamepad1_bits.start) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_HOME, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_START, true);
-            if (gamepad1_bits.select || Config::joy2cursor)
-                joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_F1 : fabgl::VirtualKey::VK_R, true);
-        }
-        else if (!(p->wButtons & XINPUT_GAMEPAD_START) && gamepad1_bits.start) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_HOME, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_START, false);
-            if (gamepad1_bits.select || Config::joy2cursor)
-                joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_F1 : fabgl::VirtualKey::VK_R, false);
-        }
-
-    gamepad1_bits.start = p->wButtons & XINPUT_GAMEPAD_START;
-
-    const uint8_t dpad = p->wButtons & 0xf;
-    bool up, down, right, left;
-    ///if (!dpad) {
-    ///    up = p->sThumbLY > 3 || p->sThumbRY > 3;
-    ///    down = p->sThumbLY < -3 || p->sThumbRY < -3;
-    ///    right = p->sThumbLX > 3 || p->sThumbRX > 3;
-    ///    left = p->sThumbLX < -3 || p->sThumbRX < -3;
-    ///}
-    ///else
-    {
-        down = dpad & XINPUT_GAMEPAD_DPAD_DOWN;
-        up = dpad & XINPUT_GAMEPAD_DPAD_UP;
-        left = dpad & XINPUT_GAMEPAD_DPAD_LEFT;
-        right = dpad & XINPUT_GAMEPAD_DPAD_RIGHT;
+    if (up != gamepad1_bits.up) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_UP, up);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_UP, up);
+    }
+    if (down != gamepad1_bits.down) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_DOWN, down);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_DOWN, down);
+    }
+    if (left != gamepad1_bits.left) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_LEFT, left);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_LEFT, left);
+    }
+    if (right != gamepad1_bits.right) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_RIGHT, right);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_RIGHT, right);
     }
 
-        if (up && !gamepad1_bits.up) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_UP, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_UP, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_PAGEUP : fabgl::VirtualKey::VK_UP, true);
-        }
-        else if (!up && gamepad1_bits.up) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_UP, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_UP, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_PAGEUP : fabgl::VirtualKey::VK_UP, false);
-        }
+    if (btn_a != gamepad1_bits.b) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_ENTER, btn_a);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_FIRE, btn_a);
+    }
+    if (btn_b != gamepad1_bits.a) {
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_ALTFIRE, btn_b);
+    }
+    if (btn_start != gamepad1_bits.start) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_HOME, btn_start);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_START, btn_start);
+    }
+    if (btn_back != gamepad1_bits.select) {
+        joyPushData(fabgl::VirtualKey::VK_MENU_BS, btn_back);
+        if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_SELECT, btn_back);
+    }
 
-        if (down && !gamepad1_bits.down) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_DOWN, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_DOWN, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_PAGEDOWN : fabgl::VirtualKey::VK_DOWN, true);
-        }
-        else if (!down && gamepad1_bits.down) {
-            joyPushData(fabgl::VirtualKey::VK_MENU_DOWN, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_DOWN, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_PAGEDOWN : fabgl::VirtualKey::VK_DOWN, false);
-        }
+    static bool prev_x = false, prev_y = false, prev_lb = false, prev_rb = false;
+    if (btn_x  != prev_x)  { kbdPushData(fabgl::VirtualKey::VK_JOY_X, btn_x);  prev_x  = btn_x; }
+    if (btn_y  != prev_y)  { kbdPushData(fabgl::VirtualKey::VK_JOY_Y, btn_y);  prev_y  = btn_y; }
+    if (btn_lb != prev_lb) { kbdPushData(fabgl::VirtualKey::VK_JOY_Z, btn_lb); prev_lb = btn_lb; }
+    if (btn_rb != prev_rb) { kbdPushData(fabgl::VirtualKey::VK_JOY_C, btn_rb); prev_rb = btn_rb; }
 
-        if (left && !gamepad1_bits.left) {
-            joyPushData(VIDEO::OSD & 0x04 ? fabgl::VirtualKey::VK_F9 : fabgl::VirtualKey::VK_MENU_LEFT, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_LEFT, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_BACKSPACE : fabgl::VirtualKey::VK_LEFT, true);
-        }
-        else if (!left && gamepad1_bits.left) {
-            joyPushData(VIDEO::OSD & 0x04 ? fabgl::VirtualKey::VK_F9 : fabgl::VirtualKey::VK_MENU_LEFT, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_LEFT, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_BACKSPACE : fabgl::VirtualKey::VK_LEFT, false);
-        }
-
-        if (right && !gamepad1_bits.right) {
-            joyPushData(VIDEO::OSD & 0x04 ? fabgl::VirtualKey::VK_F10 : fabgl::VirtualKey::VK_MENU_RIGHT, true);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_RIGHT, true);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_K : fabgl::VirtualKey::VK_RIGHT, true);
-        }
-        else if (!right && gamepad1_bits.right) {
-            joyPushData(VIDEO::OSD & 0x04 ? fabgl::VirtualKey::VK_F10 : fabgl::VirtualKey::VK_MENU_RIGHT, false);
-            if (Config::secondJoy != 1) joyPushData(fabgl::VirtualKey::VK_DPAD_RIGHT, false);
-            if (Config::joy2cursor) joyPushData(gamepad1_bits.select ? fabgl::VirtualKey::VK_K : fabgl::VirtualKey::VK_RIGHT, false);
-        }
-
-    gamepad1_bits.down = down;
-    gamepad1_bits.up = up;
-    gamepad1_bits.left = left;
-    gamepad1_bits.right = right;
-    /*char tmp[128];
-    sprintf(tmp, "[%02x, %02x], Type: %s, Buttons %04x, LT: %02x RT: %02x, LX: %d, LY: %d, RX: %d, RY: %d\n",
-                 dev_addr, instance, type_str, p->wButtons, p->bLeftTrigger, p->bRightTrigger, p->sThumbLX, p->sThumbLY, p->sThumbRX, p->sThumbRY);
-    draw_text(tmp, 0,0, 15,0);*/
+    gamepad1_bits.up     = up;
+    gamepad1_bits.down   = down;
+    gamepad1_bits.left   = left;
+    gamepad1_bits.right  = right;
+    gamepad1_bits.a      = btn_b;
+    gamepad1_bits.b      = btn_a;
+    gamepad1_bits.start  = btn_start;
+    gamepad1_bits.select = btn_back;
 
     tuh_xinput_receive_report(dev_addr, instance);
 }
